aaea32c94bf8ba834bb0aaf50f7d389c2701ec06
meanin9less <aoaaoadhkdwk@gmail.com>
2025-07-16 10:52:14 +0900

    first

---
+++ src/main/java/com/example/arirangtrail/ArirangtrailApplication.java
@@ -0,0 +1,13 @@
+package com.example.arirangtrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArirangtrailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArirangtrailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arirangtrail/ArirangtrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arirangtrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArirangtrailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
