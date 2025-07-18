5c4cd754951ff0f9c55dfe05a76059cb74496cca
meanin9less <aoaaoadhkdwk@gmail.com>
2025-07-18 18:44:14 +0900

    asd

--- src/main/java/com/example/arirangtrail/data/repository/ReviewphotoRepository.java
+++ src/main/java/com/example/arirangtrail/data/repository/ReviewphotoRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewphotoEntity extends CrudRepository<ReviewphotoEntity, Long> {
+public interface ReviewphotoRepository extends CrudRepository<ReviewphotoRepository, Long> {
 }
