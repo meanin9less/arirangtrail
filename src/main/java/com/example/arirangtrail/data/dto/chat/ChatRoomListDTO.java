98a169dc3b04332f81526dc7c60059d6f9507634
jojeongtae <jeongtae9324@gmail.com>
2025-07-28 21:56:04 +0900

    s3관련

--- src/main/java/com/example/arirangtrail/data/dto/chat/ChatRoomListDTO.java
+++ src/main/java/com/example/arirangtrail/data/dto/chat/ChatRoomListDTO.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 public class ChatRoomListDTO {
     private Long id;
     private String title;
-    private String subject; // ✨ 주제 필드 추가
     private String creator;
-    private long participantCount;
+    private long participantCount; // ✨ 참여 인원 수 필드 추가
 }
